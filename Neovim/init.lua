f0c73f01898838333481586b69fdb0e146cda766
Tiago Ávila <tiago.avila09@gmail.com>
2025-07-07 20:24:22 -0300

    Update init.lua

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -128,8 +128,8 @@ keymap('n', '<C-d>', '<C-d>zz', opts)
 keymap('n', '<C-u>', '<C-u>zz', opts)
 
 -- move text up and down
-keymap("v", "J", ":m .+1<CR>==", opts)
-keymap("v", "K", ":m .-2<CR>==", opts)
+keymap({ 'n', 'v' }, "J", ":m .+1<CR>==", opts)
+keymap({ 'n', 'v' }, "K", ":m .-2<CR>==", opts)
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
