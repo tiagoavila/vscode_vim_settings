8aed1ceddb7a9b5e43a6fa6f08c9f74ffb7b2614
Tiago Avila <tiago.avila09@gmail.com>
2025-11-11 11:51:08 -0300

    updated docs for nvim only settings

--- nvim/lua/plugins/nvim-only.lua
+++ nvim/lua/plugins/nvim-only.lua
@@ -9,13 +9,11 @@ vim.cmd [[
     Plug 'nvim-telescope/telescope.nvim'
     Plug 'nvim-lua/plenary.nvim'
 
-    " Manager for Registers like a clipboard manager
+    " Manager for Registers like a clipboard manager - Link: https://github.com/gennaro-tedesco/nvim-peekup
     Plug 'gennaro-tedesco/nvim-peekup'
 
-    " Neovim plugin for tagging important files
-    Plug 'nvim-tree/nvim-web-devicons'
-
     " Grapple is a plugin that aims to provide immediate navigation to important files
+    Plug 'nvim-tree/nvim-web-devicons'
     Plug 'cbochs/grapple.nvim'
 
     " Create key bindings that stick. WhichKey helps you remember your Neovim keymaps, by showing available keybindings in a popup as you type.
