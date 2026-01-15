fba8bc58a48a0e3b803aaa1b8c555870a8888558
Tiago Avila <tiago.avila09@gmail.com>
2026-01-15 09:32:23 -0300

    moved grapple (bookmark manager) to keymaps.lua

--- nvim/lua/nvim_settings/keymaps.lua
+++ nvim/lua/nvim_settings/keymaps.lua
@@ -20,7 +20,7 @@ keymap("n", "<leader>pf", "<cmd>Telescope find_files<CR>", { desc = "Project Fil
 keymap("n", "<leader>pr", "<cmd>Telescope oldfiles<CR>", { desc = "Project Recent Files" })
 keymap("n", "<leader>pb", "<cmd>Telescope buffers<CR>", { desc = "Project Buffers" })
 
--- Search and Replace mappings (r prefix)
+-- Search and Replace mappings (r prefix) - search-replace
 vim.api.nvim_set_keymap("v", "<C-s>", "<CMD>SearchReplaceWithinVisualSelection<CR>", { desc = "Search & Replace in Visual Selection" })
 vim.api.nvim_set_keymap("v", "<C-b>", "<CMD>SearchReplaceWithinVisualSelectionCWord<CR>", { desc = "Search & Replace Current Word in Visual Selection" })
 
@@ -30,3 +30,19 @@ vim.api.nvim_set_keymap("n", "<leader>rw", "<CMD>SearchReplaceSingleBufferCWord<
 vim.api.nvim_set_keymap("n", "<leader>rW", "<CMD>SearchReplaceSingleBufferCWORD<CR>", { desc = "Search & Replace Current WORD in Buffer" })
 vim.api.nvim_set_keymap("n", "<leader>re", "<CMD>SearchReplaceSingleBufferCExpr<CR>", { desc = "Search & Replace Current Expression in Buffer" })
 vim.api.nvim_set_keymap("n", "<leader>rf", "<CMD>SearchReplaceSingleBufferCFile<CR>", { desc = "Search & Replace Current File Path in Buffer" })
+
+-- Tag the current file - grapple
+vim.api.nvim_set_keymap('n', '<Leader>bmt', ':lua require("grapple").toggle()<CR>', vim.tbl_extend('force', opts, { desc = "Toggle Bookmark Tag" }))
+vim.api.nvim_set_keymap('n', '<Leader>bmT', ':lua require("grapple").tag()<CR>', vim.tbl_extend('force', opts, { desc = "Add Bookmark Tag" }))
+
+-- Toggle the tag window
+vim.api.nvim_set_keymap('n', '<Leader>bmv', ':lua require("grapple").toggle_tags()<CR>', vim.tbl_extend('force', opts, { desc = "View Bookmarks" }))
+
+-- Jump to the previous tag
+vim.api.nvim_set_keymap('n', '<Leader>bmp', ':lua require("grapple").jump("prev")<CR>', vim.tbl_extend('force', opts, { desc = "Previous Bookmark" }))
+
+-- Jump to the next tag
+vim.api.nvim_set_keymap('n', '<Leader>bmn', ':lua require("grapple").jump("next")<CR>', vim.tbl_extend('force', opts, { desc = "Next Bookmark" }))
+
+-- User command
+-- vim.keymap.set("n", "<leader>1", "<cmd>Grapple select index=1<cr>", { desc = "Jump to Bookmark 1" })
--- nvim/lua/plugins/configs/grapple.lua
+++ nvim/lua/plugins/configs/grapple.lua
@@ -6,18 +6,3 @@ require("grapple").setup {
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Tag the current file
-map('n', '<Leader>bmt', ':lua require("grapple").toggle()<CR>', vim.tbl_extend('force', opts, { desc = "Toggle Bookmark Tag" }))
-map('n', '<Leader>bmT', ':lua require("grapple").tag()<CR>', vim.tbl_extend('force', opts, { desc = "Add Bookmark Tag" }))
-
--- Toggle the tag window
-map('n', '<Leader>bmv', ':lua require("grapple").toggle_tags()<CR>', vim.tbl_extend('force', opts, { desc = "View Bookmarks" }))
-
--- Jump to the previous tag
-map('n', '<Leader>bmp', ':lua require("grapple").jump("prev")<CR>', vim.tbl_extend('force', opts, { desc = "Previous Bookmark" }))
-
--- Jump to the next tag
-map('n', '<Leader>bmn', ':lua require("grapple").jump("next")<CR>', vim.tbl_extend('force', opts, { desc = "Next Bookmark" }))
-
--- User command
--- vim.keymap.set("n", "<leader>1", "<cmd>Grapple select index=1<cr>", { desc = "Jump to Bookmark 1" })
